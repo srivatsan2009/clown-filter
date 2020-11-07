744ec923be18d11d0657e87e54490cb701f7c90f
srivatsan2009 <71771607+srivatsan2009@users.noreply.github.com>
2020-11-07 11:37:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ function setup() {
 canvas=createCanvas(400,300);
 canvas.center();
 video=createCapture(VIDEO);
+video.size(400,300);
 video.hide();
+poseNet=ml5.poseNet(video,modelloaded);
+poseNet.on('pose',getposes);
 }
 
 function draw() {
@@ -15,4 +18,16 @@ image(video,0,0,400,300);
 
 function take_snapshot() {
 save("srivatsan's clown filter image.jpg");
+}
+
+function modelloaded() {
+console.log("posenet model is initialised");
+}
+
+function getposes(results) {
+if (results.length>0) {
+console.log(results);
+console.log("nose x="+ results[0].pose.nose.x);
+console.log("nose y="+ results[0].pose.nose.y);
+}
 }
